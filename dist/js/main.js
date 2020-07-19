12fd128bdffdc5ab9287e10f00686a3ef0e2e790
Cody J Powell <codyj.powell@outlook.com>
2020-07-19 14:56:54 -0400

    added JS to make birthday show dep on radio

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,37 @@
 "use strict";
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
 $(window).load(function () {
   $('.loader').fadeOut('slow');
-}); // birthday dropdown
+}); //Are you 18? if not why are you here
+
+var birthday = document.querySelector('#birthday-please');
+var radioBtns = document.querySelectorAll('input[type="radio"]');
+
+var _iterator = _createForOfIteratorHelper(radioBtns),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var button = _step.value;
+    button.addEventListener('click', function () {
+      if (this.value === 'no') {
+        birthday.style.display = 'none';
+      } else {
+        birthday.style.display = '';
+      }
+    });
+  } // birthday dropdown
+
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,18 @@ $(window).load(function () {
 	$('.loader').fadeOut('slow');
 });
 
+//Are you 18? if not why are you here
+
+let birthday = document.querySelector('#birthday-please');
+const radioBtns = document.querySelectorAll('input[type="radio"]');
+for (const button of radioBtns) {
+	button.addEventListener('click', function () {
+		if (this.value === 'no') {
+			birthday.style.display = 'none';
+		} else {
+			birthday.style.display = '';
+		}
+	});
+}
+
 // birthday dropdown
